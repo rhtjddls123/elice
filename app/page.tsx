6642fff714acfb6d082286eb48cc9903a4daf851
rhtjddls123 <jjjk0605@naver.com>
2024-02-09 00:30:28 +0900

    Design: CourseArea padding top 추가
    CourseArea를 감싸고있는 div태그 padding top 16px추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,18 @@
-import Chip from './_components/Chip';
 import CourseArea from './_components/CourseArea';
+import FilterArea from './_components/FilterArea';
 import SearchArea from './_components/SearchArea';
 
 export default function Home() {
   const arr = new Array(20).fill(0);
   return (
     <>
-      <div>
-        <SearchArea></SearchArea>
-        <div className=' bg-white'>
-          <Chip>sds</Chip>
-        </div>
-        <div className=' flex flex-row flex-wrap gap-8 justify-center'>
-          {arr.map((a) => (
-            <CourseArea key={a}></CourseArea>
-          ))}
-          di
-        </div>
+      <SearchArea></SearchArea>
+      <FilterArea></FilterArea>
+      <div className=' flex flex-row flex-wrap gap-8 justify-center pt-4'>
+        {arr.map((a) => (
+          <CourseArea key={a}></CourseArea>
+        ))}
+        di
       </div>
     </>
   );
