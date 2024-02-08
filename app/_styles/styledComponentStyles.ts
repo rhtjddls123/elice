f1f3618470f15f03dc05d7824634c80491ae9350
rhtjddls123 <jjjk0605@naver.com>
2024-02-08 20:10:56 +0900

    Feat: CourseArea 생성
    courseCard와 그 안에 들어갈 내용인 CardBody styledComponent로 생성

--- app/_styles/styledComponentStyles.ts
+++ app/_styles/styledComponentStyles.ts
@@ -11,3 +11,19 @@ export const SearchInput = styled.input`
   padding-top: 12px;
   padding-bottom: 12px;
 `;
+
+export const CourseCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  width: 296px;
+  height: 338px;
+  border-radius: 8px;
+`;
+
+export const CardBody = styled.div`
+  padding-top: 28px;
+  padding-bottom: 28px;
+  padding-left: 24px;
+  padding-right: 24px;
+`;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+import CourseArea from './_components/CourseArea';
 import SearchArea from './_components/SearchArea';
 
 export default function Home() {
+  const arr = new Array(20).fill(0);
   return (
     <>
       <div>
         <SearchArea></SearchArea>
+        <div className=' flex flex-row flex-wrap gap-8 justify-center'>
+          {arr.map((a) => (
+            <CourseArea key={a}></CourseArea>
+          ))}
+        </div>
       </div>
     </>
   );
