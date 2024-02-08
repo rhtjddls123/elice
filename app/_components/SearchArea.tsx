3c2763bf6bb06029cd8c0ba37b65799105a3bd22
rhtjddls123 <jjjk0605@naver.com>
2024-02-09 03:56:57 +0900

    Feat: SearchArea 새로고침해도 input에 검색기록 남도록 수정
    1. input받는 value를 state에서 ref로 변경
    2. useEffect사용하여 처음 mount된 후 searchParams에 title이 있으면
       입력창에 띄워줌

--- app/_components/SearchArea.tsx
+++ app/_components/SearchArea.tsx
@@ -3,12 +3,12 @@
 import Image from 'next/image';
 import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import { SearchInput } from '../_styles/styledComponentStyles';
 import { debounce } from '../_utils/debounce';
 
 const SearchArea = () => {
-  const [search, setSearch] = useState('');
+  const searchRef = useRef<HTMLInputElement>(null);
   const searchParams = useSearchParams();
   const router = useRouter();
   const params = new URLSearchParams(searchParams?.toString());
@@ -16,6 +16,7 @@ const SearchArea = () => {
   // eslint-disable-next-line
   const debouncedSearch = useCallback(
     debounce((searchType: string, value: string) => {
+      console.log(value);
       if (value) {
         params.set(searchType, value);
         router.push(`?${params.toString()}`);
@@ -26,6 +27,12 @@ const SearchArea = () => {
     }, 300),
     []
   );
+
+  useEffect(() => {
+    if (params.has('title') && searchRef.current)
+      searchRef.current.value = '' + params.get('title');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className=' w-full border border-[#c9cacc] rounded bg-white my-3 flex'>
@@ -38,11 +45,11 @@ const SearchArea = () => {
       ></Image>
       <SearchInput
         placeholder='배우고싶은 언어, 기술을 검색해 보세요'
-        value={search}
-        onChange={(e) => {
-          const value = e.target.value;
-          setSearch(value);
-          debouncedSearch('title', value);
+        // value={search}
+        ref={searchRef}
+        onChange={() => {
+          const value = searchRef.current?.value;
+          debouncedSearch('title', '' + value);
         }}
       ></SearchInput>
     </div>
